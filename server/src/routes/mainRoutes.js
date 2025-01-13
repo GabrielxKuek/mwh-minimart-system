b1e1a5f8e7e1fde00a4ae22edbbbc85ab2c346a1
GabrielxKuek <kuekguanjiegabriel@gmail.com>
2025-01-13 23:40:47 +0800

    fix: rename mainRoute to mainRoutes typo

--- server/src/routes/mainRoutes.js
+++ server/src/routes/mainRoutes.js
@@ -5,16 +5,12 @@ const router = express.Router();
 // the routes just for you guys' reference. i made in year 1, might not be v optimised LOL
 ////////////////////////////////////////////////////////////
 
-// // define routes
-// const userRoute = require("./userRoute");
-// const taskRoute = require("./taskRoute");
-// const progressRoute = require("./taskProgressRoute");
-// const eastereggRoute = require("./eastereggRoute");
-// const forumRoute = require("./forumRoute"); 
+// define routes
+const taskRoute = require("./taskRoute");
 
 // // use routes
 // router.use("/users", userRoute);
-// router.use("/tasks", taskRoute);
+router.use("/tasks", taskRoute);
 // router.use("/task_progress", progressRoute);
 // router.use("/forum", forumRoute);
 
